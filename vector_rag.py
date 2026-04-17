e5d4d22667ac10826885117ac4db026825a64a78
Kapil <kydv901@gmail.com>
2026-04-17 16:55:40 +0530

    Update vector_rag.py

--- vector_rag.py
+++ vector_rag.py
@@ -1,6 +1,6 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.vectorstores import FAISS
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_huggingface import HuggingFacePipeline
